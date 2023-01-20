2b7d0e5ea62711395b6d5f632c8cb2394d07e7b1
ella <yanzi19841227@gmail.com>
2023-01-20 21:43:00 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT);
+    const API_URL = import.meta.env.VITE_API;
+    this.$http.get(API_URL).then((res) => {
+      this.data = res.data.results[0];
+    });
+  },
+};
+</script>
